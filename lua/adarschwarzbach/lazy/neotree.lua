4a16f42780124783191ca8ac0df04b8af4a9c415
adarschwarzbach <adar.schwarzbach@gmail.com>
2025-02-25 19:55:57 -0800

    show dot files in neotree

--- lua/adarschwarzbach/lazy/neotree.lua
+++ lua/adarschwarzbach/lazy/neotree.lua
@@ -1,10 +1,22 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          -- Set these to false to see all “hidden” files:
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_hidden = false,
+        },
+      },
+    })
+  end
 }
+
